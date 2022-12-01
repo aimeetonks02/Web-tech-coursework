f5dc574526d3e78f4b031844668b7cfc5e2183af
Aimee Tonks <aimztonks@gmail.com>
2022-12-01 15:02:46 +0000

    login system done???

--- login.php
+++ login.php
@@ -1,4 +1,22 @@
 <?php
-$handle = fopen("login.txt", "r");
+session_start();
 
+if (($handle = fopen("users.csv", "r")) !== FALSE){
+    while (($data = fgetcsv($handle, 100, ",")) !== FALSE){
+        $users[$data[0]] = array("password"=>$data[1], "admin"=>$data[2]);
+    }
+    fclose($handle);
+}
+
+$u = $_POST['username'];
+$p = $_POST['password'];
+
+if(isset($users[$u]) and $users[$u]['password']==$p){
+    $_SESSION['loggedin'] = TRUE;
+    $_SESSION['username'] = $u;
+    header("Location: processing.html");
+}
+else{
+    header("Location: login.html");
+}
 ?>
