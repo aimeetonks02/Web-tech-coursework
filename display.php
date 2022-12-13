b68c39bbd9c1c2e88ccd1711cb074447a82031a9
Aimee Tonks <aimztonks@gmail.com>
2022-12-13 11:26:44 +0000

    final touch-ups before submission

--- display.php
+++ display.php
@@ -1,3 +1,18 @@
 <?php
+/*
+$item = the item that was clicked on the table
 
+$fp = fopen('data.csv', 'r');
+
+$list = array($fp);
+
+foreach ($list as $i){
+    if($list[$i][$i-1]==$item){
+        display only that item on the page
+    }
+    else{
+        $i=$i+1
+    }
+};
+*/
 ?>
